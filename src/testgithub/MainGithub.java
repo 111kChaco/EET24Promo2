059f97a92874511c17d4a5f6d12a699aac7da609
Guido Benedetti <guido.benedetti@bairesdev.com>
2017-10-25 20:21:33 -0300

    Test stash

--- src/testgithub/MainGithub.java
+++ src/testgithub/MainGithub.java
@@ -3,5 +3,6 @@ package testgithub;
 public class MainGithub {
     public void main(String[] args) {
         System.out.println("Hola GitHub!!!");
+        System.out.println("Prueba de stash");
     }
 }
